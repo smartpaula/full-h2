2943bab0dc130e5ce82c016cb4f97fb51dbf6a56
a <a@a>
2024-12-20 15:45:41 +0800

    first commit

---
+++ backend/src/main/java/com/example/backend/repository/ProductRepository.java
@@ -0,0 +1,11 @@
+package com.example.backend.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.example.backend.model.Product;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long>{
+
+    
+} 
